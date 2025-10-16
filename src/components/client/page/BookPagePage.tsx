4b871365b26c527a18fbb6da9012f4b5a348979b
Toktobekova-Nora-dev <nuraiymtoktobekkyzy507@gmail.com>
2025-10-16 17:14:50 +0600

    f

--- src/components/client/page/BookPagePage.tsx
+++ src/components/client/page/BookPagePage.tsx
@@ -1,69 +1,28 @@
 import React from "react";
 import scss from "./BookPagePage.module.scss";
-
-const destinations = [
-  {
-    name: "Bishkek",
-    img: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcQP8ltVzTgyt31grKtiT_75bWBBDwPqm8qb2Z4q784aH2kb3kVeZgbLaZTAOLLRzzpT2jGtwBcyCjm8bAzRVXpI8mxald0r",
-  },
-  {
-    name: "Issyk-Kul",
-    img: "https://www.advantour.com/img/kyrgyzstan/bishkek/ala-too-square10.jpg",
-  },
-  {
-    name: "Karakol",
-    img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2a/c7/90/94/caption.jpg?w=500&h=400&s=1",
-  },
-  {
-    name: "Ala-Archa",
-    img: "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcR4j9N6CuZr0-HJrvGbupz6vCkCRoSb_FGg2OXHgb5FzLEkNz49INtBwdAHMxlOMKFnRGVzQgdw-sb_NzSoUFZRDLl6YcTU",
-  },
-];
-
 const BookPagePage = () => {
   return (
-    <div className={scss.wrapper}>
-      {/* HERO */}
-      <section className={scss.hero}>
-        <div className={scss.heroOverlay}></div>
-        <div className={scss.heroContent}>
-          <h1>Kyrgyzstan</h1>
-          <p>
-            Explore breathtaking mountains, turquoise lakes, and ancient culture
-            in the heart of Central Asia.
-          </p>
-          <div className={scss.buttons}>
-            <button className={scss.primary}>Book Hotel</button>
-            <button className={scss.outline}>Routes</button>
-            <button className={scss.outline}>Add to Favorites</button>
-          </div>
+    <section
+      className={scss.hero}
+      style={{
+        backgroundImage: `url(${"https://lh3.googleusercontent.com/gps-cs-s/AC9h4noaSIccV-cKtuIacJoRM5HCiVDWjI-BMXzPXfcOo2iXUXs0kn2iiT5NH5Z4m_dCj9IH9HJfl5TxhxpRl3e-Fu4N4pAR4JY3m81209hUyaLuejYwpmdyQ0aFvTOWxXQ98KI0deWk=s680-w680-h510-rw"})`,
+        width: "100%",
+        height: "100vh",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className={scss.hero_block}>
+        <div className={scss.box}>
+          <h1>
+            Unique travel
+            <br />
+            experiences
+          </h1>
+          <button className={scss.btn}>Learn more</button>
         </div>
-      </section>
-
-      {/* INFO BAR */}
-      <section className={scss.infoBar}>
-        <div className="container">
-          <span>☀️ 22°C</span>
-          <span>🗓 May–September</span>
-          <span>🗣 Kyrgyz / Russian</span>
-          <span>💰 Som (KGS)</span>
-          <span>🕒 GMT+6</span>
-        </div>
-      </section>
-
-      {/* POPULAR DESTINATIONS */}
-      <section className="container">
-        <h2 className={scss.sectionTitle}>Popular Destinations</h2>
-        <div className={scss.cards}>
-          {destinations.map((dest, i) => (
-            <div key={i} className={scss.card}>
-              <img src={dest.img} alt={dest.name} />
-              <h3>{dest.name}</h3>
-            </div>
-          ))}
-        </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
